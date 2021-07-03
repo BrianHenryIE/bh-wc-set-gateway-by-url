5053eb4559bf2ab8d2c9c68a3947e0a63ac660d5
Brian Henry <BrianHenryIE@gmail.com>
2021-07-02 18:13:44 -0700

    Mozart -> Strauss

--- src/autoload.php
+++ src/autoload.php
@@ -15,27 +15,25 @@ namespace BrianHenryIE\WC_Set_Gateway_By_URL;
 
 use BrianHenryIE\WC_Set_Gateway_By_URL\Pablo_Pacheco\WP_Namespace_Autoloader\WP_Namespace_Autoloader;
 
+require_once __DIR__ . '/strauss/autoload.php';
 
-// The plugin-scoped namespace for composer required libraries, as specified in composer.json Mozart config.
-$dep_namespace = 'BH_WC_Set_Gateway_By_URL';
-// The Mozart config `dep_directory` adjusted for relative path.
-$dep_directory = '/vendor/';
+$class_map_file = __DIR__ . '/autoload-classmap.php';
+if ( file_exists( $class_map_file ) ) {
 
-spl_autoload_register(
-	function ( $namespaced_class_name ) use ( $dep_namespace, $dep_directory ) {
+	$class_map = include $class_map_file;
 
-		$autoload_directory = __DIR__ . $dep_directory . '/';
+	if ( is_array( $class_map ) ) {
+		spl_autoload_register(
+			function ( $classname ) use ( $class_map ) {
 
-		// The class name with its true namespace.
-		$bare_namespaced_class_name = preg_replace( "#$dep_namespace\\\*#", '', $namespaced_class_name );
-
-		$file_path = $autoload_directory . str_replace( '\\', '/', $bare_namespaced_class_name ) . '.php';
-
-		if ( file_exists( $file_path ) ) {
-			require_once $file_path;
-		}
+				if ( array_key_exists( $classname, $class_map ) && file_exists( $class_map[ $classname ] ) ) {
+					require_once $class_map[ $classname ];
+				}
+			}
+		);
 	}
-);
+}
+unset( $class_map_file, $class_map );
 
 $wpcs_autoloader = new WP_Namespace_Autoloader();
 $wpcs_autoloader->init();
