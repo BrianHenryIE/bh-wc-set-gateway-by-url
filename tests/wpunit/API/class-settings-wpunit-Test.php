ba18750a110d797b47bd53425124c59dc72836e8
Brian Henry <BrianHenryIE@gmail.com>
2022-10-23 23:21:44 -0700

    Check plugin version in: header, define, and settings.php

--- tests/wpunit/API/class-settings-wpunit-Test.php
+++ tests/wpunit/API/class-settings-wpunit-Test.php
@@ -24,9 +24,17 @@ class Settings_WP_Unit_Test extends \Codeception\TestCase\WPTestCase {
 		$plugin_data = get_plugin_data( "$plugin_root_dir/{$settings->get_plugin_slug()}.php", false, false );
 
 		$this->assertEquals( $settings->get_plugin_version(), $plugin_data['Version'] );
-		$this->assertEquals( BH_WC_SET_GATEWAY_BY_URL_VERSION, $plugin_data['Version'] );
-		$this->assertEquals( $settings->get_plugin_version(), BH_WC_SET_GATEWAY_BY_URL_VERSION );
 
+        $plugin_file = file_get_contents( $plugin_root_dir . '/bh-wc-set-gateway-by-url.php' );
+
+        if( 1 !== preg_match('/define\( \'BH_WC_SET_GATEWAY_BY_URL_VERSION\', \'(\d+\.\d+\.\d+)\' \);/', $plugin_file, $output_array) ) {
+            $this->fail();
+        }
+
+        $bh_wc_set_gateway_by_url_version = $output_array[1];
+
+        $this->assertEquals( $bh_wc_set_gateway_by_url_version, $plugin_data['Version'] );
+        $this->assertEquals( $settings->get_plugin_version(), $bh_wc_set_gateway_by_url_version );
 	}
 
 }
