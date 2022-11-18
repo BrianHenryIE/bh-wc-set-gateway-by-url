34036187d410dd1b2af299fa58253bd215f4c080
Brian Henry <BrianHenryIE@gmail.com>
2022-11-17 21:21:58 -0800

    `use` namespace

--- autoload.php
+++ autoload.php
@@ -13,6 +13,8 @@
 
 namespace BrianHenryIE\WC_Set_Gateway_By_URL;
 
+use BrianHenryIE\WC_Set_Gateway_By_URL\Alley_Interactive\Autoloader\Autoloader;
+
 $class_map_files = array(
 	__DIR__ . '/autoload-classmap.php',
 );
@@ -38,7 +40,7 @@ unset( $class_map_files, $class_map_file, $class_map );
 // Load strauss classes after autoload-classmap.php so classes can be substituted.
 require_once __DIR__ . '/vendor-prefixed/autoload.php';
 
-\BrianHenryIE\WC_Set_Gateway_By_URL\Alley_Interactive\Autoloader\Autoloader::generate(
+Autoloader::generate(
 	'BrianHenryIE\\WC_Set_Gateway_By_URL',
 	__DIR__ . '/src',
 )->register();
